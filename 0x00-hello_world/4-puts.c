bc9c7625e2e76df7b0c6647bfce31df9673bd35b
Xavvy04 <elmehdicharib@gmail.com>
2023-10-05 14:20:01 -0700

    to styp

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
-/*
- * main - main block
- * Description: 'what?'
- * Return: 0
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
  */
 int main(void)
 {
